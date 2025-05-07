31ad1f549bfb233951787cc44b8db6b15223b2a4
Joey Cherisea <hai.p@northeastern.edu>
2025-05-07 16:22:31 -0700

    Create a util script to store all utility functions

---
+++ util.py
@@ -0,0 +1,19 @@
+"""
+    A set of utility functions for supporting data analytics 
+"""
+
+from datetime import datetime
+import re
+
+def clean_timestamp(ts_str):
+    """
+        Clean firestore generated timestamp and convert it to datetime object
+    """
+
+    # Replace Z with +00:00 for timezone compatibility
+    ts_str = ts_str.replace("Z", "+00:00")
+
+    # Regex to truncate fractional seconds to 6 digits
+    ts_str = re.sub(r'\.(\d{6})\d+', r'.\1', ts_str)
+
+    return datetime.fromisoformat(ts_str)
